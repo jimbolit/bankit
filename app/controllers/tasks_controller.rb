0a7c452d193527b954a2b13a378025195f3d2531
jimbolit <jameslittle@outlook.co.nz>
2020-04-08 16:43:34 +1200

    final commit for now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,30 @@ class TasksController < ApplicationController
 
     def index
     end
-    
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new()
+
+        if @task.save
+            redirect_to @task
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+
+    private
+
+    def task_params
+     params.required(:task).permit(:title, :content)
+    end
+
 end
